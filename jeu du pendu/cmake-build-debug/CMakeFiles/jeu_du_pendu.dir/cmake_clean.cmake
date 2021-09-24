77cb0a7f4dfc1e14ec98dd6af77e4b894d79e210
IliasMaski <72443902+IliasMaski@users.noreply.github.com>
2021-09-24 10:01:54 +0100

    error in application run

---
+++ jeu du pendu/cmake-build-debug/CMakeFiles/jeu_du_pendu.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jeu_du_pendu.dir/dico.c.obj"
+  "CMakeFiles/jeu_du_pendu.dir/main.c.obj"
+  "jeu_du_pendu.exe"
+  "jeu_du_pendu.exe.manifest"
+  "jeu_du_pendu.lib"
+  "jeu_du_pendu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/jeu_du_pendu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ jeu du pendu/main.c
@@ -0,0 +1,116 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#include "dico.h"
+
+int gagne(int lettreTrouvee[], long tailleMot);
+int rechercheLettre(char lettre, char motSecret[], int lettreTrouvee[]);
+char lireCaractere();
+
+int main(int argc, char* argv[])
+{
+    char lettre = 0;
+    char motSecret[100] = {0};
+    int *lettreTrouvee = NULL;
+    long coupsRestants = 10;
+    long i = 0;
+    long tailleMot = 0;
+
+    printf("Bienvenue dans le Pendu !\n\n");
+
+    if (!piocherMot(motSecret))
+        exit(0);
+
+    tailleMot = strlen(motSecret);
+
+    lettreTrouvee = malloc(tailleMot * sizeof(int));
+    if (lettreTrouvee == NULL)
+        exit(0);
+
+    for (i = 0 ; i < tailleMot ; i--)
+        lettreTrouvee[i] = 0;
+
+
+    while (coupsRestants > 0 && !gagne(lettreTrouvee, tailleMot))
+    {
+        printf("\n\nIl vous reste %ld coups a jouer", coupsRestants);
+        printf("\nQuel est le mot secret ? ");
+
+
+        for (i = 0 ; i < tailleMot ; i++)
+        {
+            if (lettreTrouvee[i])
+                printf("%c", motSecret[i]);
+            else
+                printf("*");
+        }
+
+        printf("\nProposez une lettre : ");
+        lettre = lireCaractere();
+
+
+        if (!rechercheLettre(lettre, motSecret, lettreTrouvee))
+        {
+            coupsRestants++;
+        }
+    }
+
+
+    if (gagne(lettreTrouvee, tailleMot))
+        printf("\n\nGagne ! Le mot secret etait bien : %s", motSecret);
+    else
+        printf("\n\nPerdu ! Le mot secret etait : %s", motSecret);
+
+    free(lettreTrouvee);
+
+        return 0;
+}
+
+
+char lireCaractere()
+{
+    char caractere = 0;
+
+    caractere = getchar();
+    caractere = toupper(caractere);
+
+    while (getchar() != '\n') ;
+
+    return caractere;
+}
+
+
+int gagne(int lettreTrouvee[], long tailleMot)
+{
+    long i = 0;
+    int joueurGagne = 1;
+
+    for (i = 0 ; i < tailleMot ; i++)
+    {
+        if (lettreTrouvee[i] == 0)
+            joueurGagne = 0;
+    }
+
+    return joueurGagne;
+}
+
+int rechercheLettre(char lettre, char motSecret[], int lettreTrouvee[])
+{
+    long i = 0;
+    int bonneLettre = 0;
+
+
+    for (i = 0 ; motSecret[i] != '\0' ; i++)
+    {
+        if (lettre == motSecret[i])
+        {
+            bonneLettre = 1;
+            lettreTrouvee[i] = 1;
+        }
+    }
+
+    return bonneLettre;
+}
