60462fd012e719b4b53bd9039c3b697bb36c5c6e
Ilias Maski <iliasmaski.im@gmail.com>
2021-09-28 22:52:41 +0100

    errors left

---
+++ cmake-build-debug/CMakeFiles/pendu_game.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pendu_game.dir/dico.c.obj"
+  "CMakeFiles/pendu_game.dir/main.c.obj"
+  "pendu_game.exe"
+  "pendu_game.exe.manifest"
+  "pendu_game.lib"
+  "pendu_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pendu_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
